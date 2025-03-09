44bfb06ed851202a7bdaaea96955e2f8493d2968
amirulaimann17 <amirulaimann17@gmail.com>
2025-03-10 07:07:22 +0800

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // Enables static export
+  trailingSlash: true, // Ensures paths end with "/"
+  images: {
+    unoptimized: true, // Required if using Next.js Image component
+  },
 };
 
 export default nextConfig;
